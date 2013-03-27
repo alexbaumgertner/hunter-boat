5ec9189903daefd56a21c5bba32e4cfc3fe55986
alexbaumgertner <alexbaumgertner@yandex.ru>
2013-03-27 19:22:09 +0400

    add faq js

--- desktop.blocks/faq/faq.js
+++ desktop.blocks/faq/faq.js
@@ -8,16 +8,20 @@ BEM.DOM.decl('faq', {
     onSetMod : {
 
         'js' : function() {
-            /* ... */
+            var _this = this;
+
+            this.elem('item').on('click', function () {
+                _this.toggleMod($(this), 'state', 'opened', 'closed');
+            });
         }
 
     }
 
 }, {
-
+/*
     live : function() {
-        /* ... */
-    }
+        *//* ... *//*
+    }*/
 
 });
 
--- desktop.bundles/faq/faq.bemjson.js
+++ desktop.bundles/faq/faq.bemjson.js
@@ -179,6 +179,7 @@
                                 content : [
                                     {
                                         block: 'faq',
+                                        js: true,
                                         content: [
                                             {
                                                 elem: 'top',
