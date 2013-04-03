6991505edcdc3c83cfe7ad641db616e344a52b77
alexbaumgertner <alexbaumgertner@yandex.ru>
2013-04-04 00:45:43 +0400

    add blocks: delivery-info, shipping-calc; add b-text list-type blue-circle

---
+++ desktop.blocks/shipping-calc/shipping-calc.js
@@ -0,0 +1,24 @@
+/** @requires BEM */
+/** @requires BEM.DOM */
+
+(function(undefined) {
+
+BEM.DOM.decl('shipping-calc', {
+
+    onSetMod : {
+
+        'js' : function() {
+            /* ... */
+        }
+
+    }
+
+}, {
+
+    live : function() {
+        /* ... */
+    }
+
+});
+
+})();
