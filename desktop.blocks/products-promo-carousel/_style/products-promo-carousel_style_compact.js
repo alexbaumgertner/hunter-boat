410420fa8f1f94ef30401ae4adeb62bdc04f171d
alexbaumgertner <alexbaumgertner@yandex.ru>
2013-04-11 11:54:18 +0400

    edit blocks: header, add mod page index, elem index-title; add products-promo-carousel mod style compact

---
+++ desktop.blocks/products-promo-carousel/_style/products-promo-carousel_style_compact.js
@@ -0,0 +1,24 @@
+/** @requires BEM */
+/** @requires BEM.DOM */
+
+(function(undefined) {
+
+BEM.DOM.decl({ block: 'products-promo-carousel', modName: 'style', modVal: 'compact'}, {
+
+    onSetMod : {
+
+        'js' : function() {
+            /* ... */
+        }
+
+    }
+
+}, {
+
+    live : function() {
+        /* ... */
+    }
+
+});
+
+})();
