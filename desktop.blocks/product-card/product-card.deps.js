76e6398b3fbdf5b1529e86b8177699537103e0e1
alexbaumgertner <alexbaumgertner@yandex.ru>
2013-04-12 15:42:39 +0400

    fix catalog page and product card

--- desktop.blocks/product-card/product-card.deps.js
+++ desktop.blocks/product-card/product-card.deps.js
@@ -2,7 +2,7 @@
     shouldDeps: [
         {
             block: 'product-card',
-            elems: [ 'photo', 'title', 'desc', 'price', 'icon' ]
+            elems: [ 'info', 'photo', 'title', 'desc', 'price', 'icon' ]
         },
         {
             block: 'price'
