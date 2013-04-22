1be8b66e628a9a299bd2aa599ae0148ceb92d33f
alexbaumgertner <alexbaumgertner@yandex.ru>
2013-04-22 13:38:51 +0400

    cancel delete block company brief

---
+++ desktop.blocks/company-brief/company-brief.js
@@ -0,0 +1,24 @@
+/** @requires BEM */
+/** @requires BEM.DOM */
+
+(function(undefined) {
+
+BEM.DOM.decl('company-brief', {
+
+    onSetMod : {
+
+        'js' : function() {
+            /* ... */
+        }
+
+    }
+
+}, {
+
+    live : function() {
+        /* ... */
+    }
+
+});
+
+})();
