d20ad8c58cc237e3c2715a8fbbc05da65fde9a93
alexbaumgertner <alexbaumgertner@yandex.ru>
2013-04-24 13:33:50 +0400

    fixes

--- desktop.blocks/catalog-page/catalog-page.js
+++ desktop.blocks/catalog-page/catalog-page.js
@@ -1,28 +1,60 @@
 /** @requires BEM */
 /** @requires BEM.DOM */
 
-(function(undefined) {
+(function (undefined) {
 
 BEM.DOM.decl('catalog-page', {
 
-    onSetMod : {
+        onSetMod: {
 
-        'js' : function() {
-            var _this = this;
+            'js': function () {
 
-            this.elem('menu-item-title').click(function () {
-                _this.toggleMod($(this), 'order', 'asc', 'desc');
+            }
+
+        },
+
+        onElemSetMod: {
+
+            'menu-item': {
+
+                'state': {
+
+                    'current': function (elem, modName, modVal, oldModVal) {
+
+                        if (oldModVal == 'disabled') {
+                            return false;
+                        }
+
+                        var prev = this.elem('menu-item', 'state', 'current');
+                        this.delMod(prev, 'state').trigger('current', {
+                            prev: prev,
+                            current: elem
+                        });
+                    }
+                }
+            }
+        },
+
+        onItemSelectorClick: function (e) {
+            var item = this._getItemByEvent(e);
+            this.setMod(item, 'state', 'current');
+        },
+
+
+        _getItemByEvent: function (e) {
+            return e.data.domElem.closest(this.buildSelector('menu-item'));
+        }
+
+    },
+
+    {
+
+        live : function() {
+            this.liveBindTo('menu-item', 'leftclick', function(e) {
+                this.onItemSelectorClick(e);
             });
         }
 
-    }
-
-}, {
-
-    /*live : function() {
-        *//* ... *//*
-    }*/
-
-});
+    });
 
 })();
