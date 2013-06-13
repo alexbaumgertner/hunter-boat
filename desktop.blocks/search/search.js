217a634f5f5ef21f868ed2861ba842333bc958a1
alexbaumgertner <alexbaumgertner@yandex.ru>
2013-06-13 15:55:49 +0400

    fixes

--- desktop.blocks/search/search.js
+++ desktop.blocks/search/search.js
@@ -8,31 +8,35 @@
         onSetMod: {
 
             'js': function () {
+
+                var _this = this;
+
                 (function (w, d, c) {
-
-                    // check js avaible
-                    var e = d.documentElement;
-                    (' ' + e.className + ' ').indexOf(' ya-page_js_yes ') === -1 && (e.className += ' ya-page_js_yes');
-
-                    // load search api
-                    if (!d.getElementById('site_yandex_net_v2_0_js_all')) {
-                        var s = d.createElement('script'), h = d.getElementsByTagName('script')[0];
-                        s.type = 'text/javascript';
-                        s.id = 'site_yandex_net_v2_0_js_all';
-                        s.async = true;
-                        s.charset = 'utf-8';
-                        s.src = (d.location.protocol === 'https:' ? 'https:' : 'http:') + '//site.yandex.net/v2.0/js/all.js';
-                        h.parentNode.insertBefore(s, h);
-                    }
-
 
                     (w[c] || (w[c] = [])).push(function () {
                         Ya.Site.Form.init();
-                    })
+                    });
+
+                    _this.loadApi(w, d, c);
 
                 })(window, document, 'yandex_site_callbacks');
             }
 
+        },
+
+
+        loadApi: function (w, d, c) {
+            // load search api
+            if (!d.getElementById('site_yandex_net_v2_0_js_all')) {
+                var s = d.createElement('script'), h = d.getElementsByTagName('script')[0];
+
+                s.type = 'text/javascript';
+                s.id = 'site_yandex_net_v2_0_js_all';
+                s.async = true;
+                s.charset = 'utf-8';
+                s.src = (d.location.protocol === 'https:' ? 'https:' : 'http:') + '//site.yandex.net/v2.0/js/all.js';
+                h.parentNode.insertBefore(s, h);
+            }
         }
 
     }, {
