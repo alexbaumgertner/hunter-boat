56c48b743d48ff85eefc23433496013d91899efe
alexbaumgertner <alexbaumgertner@yandex.ru>
2013-07-09 13:10:21 +0400

    Анимация элемента "добавление в корзину" блока "карточка товара"

--- desktop.blocks/catalog-item-card/catalog-item-card.js
+++ desktop.blocks/catalog-item-card/catalog-item-card.js
@@ -30,46 +30,65 @@ BEM.DOM.decl('catalog-item-card', {
         'added-in-cart': {
             'visible': {
                 'yes': function (elem, modName, modVal, oldModVal) {
-                    var _this = this;
-                    var targetTopMargin = 16;
+                    this.showAddToCardAnimation(elem);
+                },
 
-                    var beforeAnimateTime = 1000; // ms
+                'no': function (elem, modName, modVal, oldModVal) {
+                    this.afterCurrentEvent(
+                        function(){
+                            this.domElem.submit();
+                        }
+                    );
 
-                    var offsetBuyButton = this.elem('buy-button').offset();
-                    var positionX = offsetBuyButton.left + (this.elem('buy-button').outerWidth() / 2) - (elem.outerWidth() / 2);
-                    var positionY = offsetBuyButton.top + (this.elem('buy-button').outerHeight() / 2) - (elem.outerHeight() / 2);
-
-
-                    elem.css({ left: positionX, top: positionY })
-                        .promise()
-                        .done(function(){
-                            var targetDOMElem = _this.findBlockOutside('b-page').findBlockInside('cart-info').elem('link');
-
-                            // animation
-                            setTimeout(function () {
-                                elem.animate(
-                                    {   left:targetDOMElem.offset().left,
-                                        top: targetDOMElem.offset().top - targetTopMargin,
-                                        width: 0,
-                                        height: 0,
-                                        padding: 0,
-                                        'font-size': 0,
-                                        'line-height': 0
-                                    }, function () {
-                                        _this.delMod(elem, 'visible');
-
-                                        // clear
-                                        elem.css({ width: '', height: '', 'font-size': '', 'line-height': '', padding: '' })
-                                    });
-                            }, beforeAnimateTime);
-
-
-                        });
                 }
             }
         }
     },
 
+    showAddToCardAnimation: function (elem) {
+        var _this = this;
+        var targetTopMargin = 16; // px
+        var beforeAnimateTime = 1000; // ms
+        var offsetBuyButton = this.elem('buy-button').offset();
+        var positionX = offsetBuyButton.left + (this.elem('buy-button').outerWidth() / 2) - (elem.outerWidth() / 2);
+        var positionY = offsetBuyButton.top + (this.elem('buy-button').outerHeight() / 2) - (elem.outerHeight() / 2);
+        var targetDOMElem = _this.findBlockOutside('b-page').findBlockInside('cart-info').elem('link');
+
+        // Show elem and..
+        elem.css(
+            {
+                left: positionX,
+                top: positionY
+            })
+            .promise()
+            // ..after show wait beforeAnimateTime, then animate.
+            .done(function(){
+                elem.delay(beforeAnimateTime)
+                    .animate({   left: targetDOMElem.offset().left,
+                        'top': targetDOMElem.offset().top - targetTopMargin,
+                        'width': 0,
+                        'height': 0,
+                        'padding': 0,
+                        'font-size': 0,
+                        'line-height': 0
+                    },
+
+                    // After animate hide this block..
+                    function () {
+
+                        _this.setMod(elem, 'visible', 'no');
+
+                        // .. and clear styles
+                        elem.css({
+                            'width': '',
+                            'height': '',
+                            'padding': '',
+                            'font-size': '',
+                            'line-height': ''
+                        });
+                    });
+            });
+    },
 
     selectColor: function (color) {
         this.elem('photo-big-img').attr('src', this.photoBigColors[color]);
@@ -90,7 +109,8 @@ BEM.DOM.decl('catalog-item-card', {
         });
 
         // add to cart
-        this.liveBindTo('buy-button', 'click', function () {
+        this.liveBindTo('buy-button', 'click', function (e) {
+            e.preventDefault();
             this.setMod(this.elem('added-in-cart'), 'visible', 'yes');
         });
     }
