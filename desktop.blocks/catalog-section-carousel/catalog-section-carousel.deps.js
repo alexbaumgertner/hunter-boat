0dc1b3d4cce56e2561327b64483a80b58335c89a
alexbaumgertner <alexbaumgertner@yandex.ru>
2013-08-05 22:07:15 +0400

    Поправлены карусели на странице товара

--- desktop.blocks/catalog-section-carousel/catalog-section-carousel.deps.js
+++ desktop.blocks/catalog-section-carousel/catalog-section-carousel.deps.js
@@ -1,6 +1,6 @@
 ({
     shouldDeps: [
         { block: 'i-font', mods: { face: 'hermes' } },
-        { mods: {'type': ['previouslyViewedProducts', 'motors'] } }
+        { mods: {'type': ['previouslyViewedProducts', 'motors', 'accessories', 'recommend'] } }
     ]
 })
